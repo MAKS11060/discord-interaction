662a6bb52f036ee383723fe633be4404d60d0445
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2024-07-27 14:54:48 +0200

    use req.bytes()

--- src/lib/ed25519.ts
+++ src/lib/ed25519.ts
@@ -4,10 +4,10 @@ import {decodeHex, encodeHex} from '@std/encoding/hex'
 const encoder = new TextEncoder()
 
 /**
- * Imports a public key from a hexadecimal string and returns a CryptoKey object.
+ * Imports a public key from a `hexadecimal` string and returns a {@linkcode CryptoKey} object.
  *
  * @param {string} publicKey - The public key in hexadecimal format.
- * @returns {Promise<CryptoKey>} A promise that resolves to a CryptoKey object representing the imported public key.
+ * @returns {Promise<CryptoKey>} A promise that resolves to a {@linkcode CryptoKey} object representing the imported public key.
  */
 export const importKeyRaw = (publicKey: string): Promise<CryptoKey> => {
   return crypto.subtle.importKey('raw', decodeHex(publicKey), 'Ed25519', false, ['verify'])
@@ -17,12 +17,12 @@ export const importKeyRaw = (publicKey: string): Promise<CryptoKey> => {
  * Verifies the signature of a {@linkcode request} using a {@linkcode CryptoKey} object.
  *
  * The function checks the `X-Signature-Ed25519` and `X-Signature-Timestamp` headers of the request,
- * and verifies the signature using the provided CryptoKey object. If the signature is invalid,
- * the function returns a Response object with a 401 status code and an error message.
+ * and verifies the signature using the provided {@linkcode CryptoKey} object. If the signature is invalid,
+ * the function returns a {@linkcode Response} object with a 401 status code and an error message.
  *
  * @param {Request} request - The request object to verify the signature of.
- * @param {CryptoKey} key - The CryptoKey object representing the public key to use for verification.
- * @returns {Promise<Response | null>} A promise that resolves to a Response object with an error message if the signature is invalid, or null if the signature is valid.
+ * @param {CryptoKey} key - The {@linkcode CryptoKey} object representing the public key to use for verification.
+ * @returns {Promise<Response | null>} A promise that resolves to a {@linkcode Response} object with an error message if the signature is invalid, or null if the signature is valid.
  */
 export const verifyRequestSignature = async (
   request: Request,
@@ -30,8 +30,6 @@ export const verifyRequestSignature = async (
 ): Promise<Response | null> => {
   const signature = request.headers.get('X-Signature-Ed25519')
   const timestamp = request.headers.get('X-Signature-Timestamp')
-  const body = new Uint8Array(await request.clone().arrayBuffer())
-
   if (signature === null || timestamp === null) {
     return Response.json({error: 'Bad request signature'}, {status: 401})
   }
@@ -40,7 +38,7 @@ export const verifyRequestSignature = async (
     key.algorithm,
     key,
     decodeHex(signature),
-    concat([encoder.encode(timestamp), body]) // (timestamp + body)
+    concat([encoder.encode(timestamp), await request.clone().bytes()]) // (timestamp + body)
   )
   if (!valid) {
     return Response.json({error: 'Bad request signature'}, {status: 401})
@@ -61,12 +59,11 @@ export const verifyRequestSignature = async (
  * @returns {Promise<Request>} A promise that resolves to a new Request object with the signature and timestamp headers added.
  */
 export const signRequest = async (req: Request, key: CryptoKey): Promise<Request> => {
-  const body = new Uint8Array(await req.clone().arrayBuffer())
   const timestamp = Math.floor(Date.now() / 1000).toString()
   const signature = await crypto.subtle.sign(
     'Ed25519',
     key,
-    concat([encoder.encode(timestamp), body]) // (timestamp + body)
+    concat([encoder.encode(timestamp), await req.clone().bytes()]) // (timestamp + body)
   )
   const newReq = new Request(req)
   newReq.headers.set('X-Signature-Ed25519', encodeHex(signature))
