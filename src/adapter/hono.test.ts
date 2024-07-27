b7ac069b2f81de9e6bfbbe68c368897d66de0030
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2024-07-27 15:38:37 +0200

    use hono methods to access cached body

--- src/adapter/hono.test.ts
+++ src/adapter/hono.test.ts
@@ -33,13 +33,23 @@ Deno.test('ping', async (c) => {
   })
 
   await c.step('bad sign', async () => {
-    const res = await app.fetch(req)
+    const res = await app.fetch(req.clone())
+    assertEquals(res.status, 401)
+    assertEquals(await res.json(), {error: 'Bad request signature'})
+  })
+
+  await c.step('without headers', async () => {
+    const signReq = await signRequest(req.clone(), keys.privateKey)
+    signReq.headers.delete('X-Signature-Ed25519')
+    signReq.headers.delete('X-Signature-Timestamp')
+    const res = await app.fetch(signReq)
     assertEquals(res.status, 401)
     assertEquals(await res.json(), {error: 'Bad request signature'})
   })
 
   await c.step('sign', async () => {
-    const res = await app.fetch(await signRequest(req, keys.privateKey))
+    const signReq = await signRequest(req.clone(), keys.privateKey)
+    const res = await app.fetch(signReq)
     assertEquals(res.status, 200)
     assertEquals(await res.json(), {type: 1})
   })
--- src/adapter/hono.ts
+++ src/adapter/hono.ts
@@ -18,25 +18,42 @@
  * ```
  */
 
+import {concat} from '@std/bytes/concat'
+import {decodeHex} from '@std/encoding/hex'
 import type {APIInteraction} from 'discord-api-types/v10'
 import type {Handler} from 'hono'
 import {createFactory} from 'hono/factory'
 import {createHandler, type CreateHandlerOptions} from '../interaction.ts'
-import {verifyRequestSignature as verifyRequest} from '../lib/ed25519.ts'
 import type {Command} from '../types.ts'
 
 export {importKeyRaw} from '../lib/ed25519.ts'
 
+const encoder = new TextEncoder()
+
 /**
- * Creates a middleware function that verifies the signature of a request using a CryptoKey object.
+ * Creates a middleware function that verifies the signature of a request using a {@linkcode CryptoKey} object.
  *
- * @param {CryptoKey} key - The CryptoKey object representing the public key to use for verification.
+ * @param {CryptoKey} key - The {@linkcode CryptoKey} object representing the public key to use for verification.
  * @returns {MiddlewareHandler} A middleware function that can be used with the Hono framework to verify the signature of a request.
  */
 const verifyRequestSignature = (key: CryptoKey) => {
   return createFactory().createMiddleware(async (c, next) => {
-    const invalid = await verifyRequest(c.req.raw, key)
-    if (invalid) return invalid
+    const signature = c.req.header('X-Signature-Ed25519')
+    const timestamp = c.req.header('X-Signature-Timestamp')
+    if (!signature || !timestamp) {
+      return Response.json({error: 'Bad request signature'}, {status: 401})
+    }
+
+    const valid = await crypto.subtle.verify(
+      key.algorithm,
+      key,
+      decodeHex(signature),
+      concat([encoder.encode(timestamp), new Uint8Array(await c.req.arrayBuffer())]) // (timestamp + body)
+    )
+    if (!valid) {
+      return Response.json({error: 'Bad request signature'}, {status: 401})
+    }
+
     await next()
   })
 }
@@ -44,11 +61,11 @@ const verifyRequestSignature = (key: CryptoKey) => {
 /**
  * Adapter for {@link https://hono.dev Hono} framework.
  *
- * Creates a set of middleware handlers for the Hono framework that can be used to handle Discord interactions.
+ * Creates a set of middleware handlers for the `Hono` framework that can be used to handle Discord interactions.
  *
  * @param {CryptoKey} key - The CryptoKey object representing the public key to use for verifying the signature of the interaction.
  * @param {Command[]} commands - An array of Command objects representing the commands that the bot supports.
- * @returns {Promise<Handler[]>} A promise that resolves to an array of middleware handlers that can be used with the Hono framework to handle Discord interactions.
+ * @returns {Promise<Handler[]>} A promise that resolves to an array of middleware handlers that can be used with the `Hono` framework to handle Discord interactions.
  *
  * @example
  * ```ts
